1fb850caf450070f1f4b214bb29cd7fd4509b0cb
leopires1 <160489844+leopires1@users.noreply.github.com>
2024-02-29 20:15:37 -0300

    Add files via upload

---
+++ Program - Ex1.cs
@@ -0,0 +1,22 @@
+﻿
+double[] vet = new double[10];
+string? valor;
+double salario;
+
+for(int i = 0; i < 10; i++)
+{
+    Console.WriteLine("Digite o salário da pessoa " + (i + 1) + ": ");
+    valor = Console.ReadLine();
+    Double.TryParse(valor, out salario);
+    vet[i] = salario;
+}
+
+double maiorSalario = 0;
+for(int i = 0; i < 10; i++){
+    if (vet[i] > maiorSalario)
+    {
+        maiorSalario = vet[i];
+    }
+}
+
+Console.WriteLine("Maior salário: R$"+ maiorSalario);
